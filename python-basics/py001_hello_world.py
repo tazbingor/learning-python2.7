c3861d38758c2fe4d386caa7e9c061ff1c3fed3b
jieyan <852353298@qq.com>
2017-10-28 11:38:25 +0800

    hello world

---
+++ python-basics/py001_hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/10/28 上午9:25
+# @Author  : Aries
+# @Site    : 
+# @File    : py001_hello_world.py
+# @Software: PyCharm
+
+print "hello world"
