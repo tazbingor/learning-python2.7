2978676a6d35144186a97120c1cd652a25663b02
jieyan <852353298@qq.com>
2017-10-29 10:53:40 +0800

    python operator

---
+++ python-basics/py003_operator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/10/29 上午10:32
+# @Author  : Aries
+# @Site    : 
+# @File    : py003_operator.py
+# @Software: PyCharm
+# 运算符
+
+
+print 18 / 5
+print 18 % 5
+
+a = 9
+print float(a)
+
+# bool值
+print bool(1)
+print bool(2)
+print bool(0)  # False
+print bool(True)
+print bool(False)  # False
+print bool(None)  # False
+print bool([])  # False
+print bool(())  # False
+
