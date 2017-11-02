78012b02c2f89cd23537dde9dc8af5fd8b6756b1
jieyan <852353298@qq.com>
2017-11-02 10:15:39 +0800

    python bool

---
+++ python-basics/py010_none.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/2 上午9:55
+# @Author  : Aries
+# @Site    : 
+# @File    : py010_none.py
+# @Software: PyCharm
+# 核心笔记 布尔值
+
+'''
+python中的虽有标准对象都是可以进行bool测试的
+'''
+
+# None
+print None
+print None == None
