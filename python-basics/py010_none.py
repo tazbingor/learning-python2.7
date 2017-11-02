e05713d99af52dae62869daa2c36a5b7c93cbf8c
jieyan <852353298@qq.com>
2017-11-02 10:26:35 +0800

    python none

--- python-basics/py010_none.py
+++ python-basics/py010_none.py
@@ -12,5 +12,22 @@ python中的虽有标准对象都是可以进行bool测试的
 '''
 
 # None
-print None
-print None == None
+print bool(None)
+print None == None  # True
+
+# 布尔型
+# 以及所有为零的数
+print bool(0)
+print bool(0.0)
+print bool(0L)
+print bool(0.0j)
+
+# 空字符串
+print bool("")
+
+# 空列表
+print bool([])
+# 空元组
+print bool(())
+# 空字典
+print bool({})
