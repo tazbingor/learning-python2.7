f000e11fb899032d7e6a620d5960518f3cc4d90c
jieyan <852353298@qq.com>
2017-11-02 16:54:36 +0800

    python number functions

---
+++ python-basics/py018_number_functions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/2 下午4:41
+# @Author  : Aries
+# @Site    : 
+# @File    : py018_number_functions.py
+# @Software: PyCharm
+'''
+数字内建函数
+'''
+
+# 四种数字类型的转换
+print int(16.04)  # 16
+print long(16l)  # 16
+print float(16)  # 16.0
+print complex(16.6)  # (16.6+0j)
+print complex(16)  # (16.6+0j)
