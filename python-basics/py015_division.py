030c5c6c7a593598bc83d200f688bda46e004fde
jieyan <852353298@qq.com>
2017-11-02 16:30:46 +0800

    python division

---
+++ python-basics/py015_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/2 下午4:20
+# @Author  : Aries
+# @Site    : 
+# @File    : py015_division.py
+# @Software: PyCharm
+
+'''
+除法
+符号"/"和'//'
+
+'''
+
+print 9 / 3  # 3
+print 9 // 3  # 3
+
+print 7 / 2  # 3
+print 7 // 2  # 3
+
+print 7 / 2.0  # 3.5
+print 7.0 / 2  # 3.5
+print 7 // 2.0  # 3.0
+print 7.0 // 2  # 3.0
+
+# print ~1
