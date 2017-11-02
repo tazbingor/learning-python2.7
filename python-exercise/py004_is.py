4473fca76fef6a58e53cb1f5fe1593675e7e2a6b
jieyan <852353298@qq.com>
2017-11-02 15:34:28 +0800

    python is

--- python-exercise/py004_is.py
+++ python-exercise/py004_is.py
@@ -24,3 +24,9 @@ f = 100.0
 print a is b  # True
 print c is d  # True
 print e is f  # True
+
+# 都为True,原因很简单,首先数字类型是属于不可改变的类型,
+# 所以当一个或者多个数字变量的值相同,那么在实例化时,这些变量会指向同一块内存区域 使用id()即可证明
+print id(a) == id(b) # True
+print id(c) == id(d) # True
+print id(e) == id(f) # True
