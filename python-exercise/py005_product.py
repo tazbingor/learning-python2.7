bde4c866c4396d311431bdfc8dc5714273ba3836
jieyan <852353298@qq.com>
2017-11-03 11:44:34 +0800

    python random functions

---
+++ python-exercise/py005_product.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/3 上午11:42
+# @Author  : Aries
+# @Site    : 
+# @File    : py005_product.py
+# @Software: PyCharm
+'''
+求俩数的乘积
+'''
+
+
+def mathProduct(number1, number2):
+    return number1 * number2
+
+
+print mathProduct(2, 2)
