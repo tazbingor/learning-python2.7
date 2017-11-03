79ff4e1da6fdc9e5142224bcea9d93fc92f5dc4e
jieyan <852353298@qq.com>
2017-11-03 16:46:59 +0800

    python limit range

---
+++ python-exercise/py014_limit_range.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/3 下午4:30
+# @Author  : Aries
+# @Site    : 
+# @File    : py014_limit_range.py
+# @Software: PyCharm
+# 显示当前系统的极限范围
+
+import sys
+
+print sys.maxint
+
+print -sys.maxint - 1
+
+print sys.float_info
+
+print sys.long_info
