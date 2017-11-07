3a39ce66b88a11d26af4ae5945cf09dd638afdb7
jieyan <852353298@qq.com>
2017-11-07 17:13:52 +0800

    python number triangle

---
+++ python-exercise/py023_number_triangle_02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/7 下午4:44
+# @Author  : Aries
+# @Site    : 
+# @File    : py023_number_triangle_02.py.py
+# @Software: PyCharm
+'''
+数字三角形
+样式2
+
+'''
+col = 0
+while col < 6:  # 小于六行
+    col += 1
+    item = 1
+    while item <= 6 - col:  # 每一行减少一个元素
+        print item,
+        item += 1
+    print
+
+print
