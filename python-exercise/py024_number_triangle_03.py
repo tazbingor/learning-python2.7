8e1a188cf737b22dd33fa0e1a2ea651430e3bcf7
jieyan <852353298@qq.com>
2017-11-07 19:43:52 +0800

    python number triangle

--- python-exercise/py024_number_triangle_03.py
+++ python-exercise/py024_number_triangle_03.py
@@ -2,11 +2,43 @@
 # -*- coding: utf-8 -*-
 # @Time    : 17/11/7 下午5:15
 # @Author  : Aries
-# @Site    : 
+# @Site    :
 # @File    : py024_number_triangle_03.py.py
 # @Software: PyCharm
 '''
 样式三
+
+int main()
+{
+ int i,j;
+ for(i=1;i<=5;++i)
+  {
+     for(j=i;j>=1;j--)
+       printf("%d",j);
+       printf("  ");
+  }
+ return 0;
+}
+
 '''
 
+i = 0
+j = 0
+num = 6
+while i < num:
+    i += 1
+    j = i
+    arr = []
+    count = 0
 
+    # 插入空格符
+    while count < num - j:
+        arr.append(" ")
+        count += 1
+
+    while j >= 1:
+        # print str(j),
+        arr.append(str(j))  # 尾部插入數值
+        j -= 1
+    result = "".join(arr)  # 數組轉字符串
+    print result
