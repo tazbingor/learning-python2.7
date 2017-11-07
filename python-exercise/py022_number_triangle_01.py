efad7fc213e2f305530faf5c885ece9d31264add
jieyan <852353298@qq.com>
2017-11-07 16:44:39 +0800

    python number triangle

---
+++ python-exercise/py022_number_triangle_01.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/7 下午4:33
+# @Author  : Aries
+# @Site    : 
+# @File    : py022_number_triangle_01.py
+# @Software: PyCharm
+'''
+数字三角形
+
+样式一
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+'''
+
+item = 1
+while item < 6:
+    item += 1
+    col = 1
+    while col < item:
+        print col,
+        col += 1
+    print
+
+print
+
+
