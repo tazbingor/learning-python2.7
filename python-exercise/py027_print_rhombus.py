2b2a99816eae53ed9260311ca93ab93e1e9ac64e
jieyan <852353298@qq.com>
2017-11-08 17:11:35 +0800

    python print rhombus

--- python-exercise/py027_print_rhombus.py
+++ python-exercise/py027_print_rhombus.py
@@ -7,7 +7,24 @@
 # @Software: PyCharm
 '''
 输出菱形
+
+
 '''
-result = '' + str(1)
-print result
-print type(result)
+
+star = 1
+num = int(raw_input("请输入15以内的数\n"))
+col = num
+i = 0
+while i < num:  # 上金字塔
+    print ' ' * col, '*' * star
+    col -= 1
+    star += 2
+    i += 1
+
+col = 0
+j = 0
+while j < num + 1:  # 逆金字塔
+    print ' ' * col, '*' * star
+    col += 1
+    star -= 2
+    j += 1
