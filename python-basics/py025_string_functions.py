09bcf0d7aa4693e636cfd9c6197fcfc861a87973
jieyan <852353298@qq.com>
2017-11-09 22:22:58 +0800

    python string functions

---
+++ python-basics/py025_string_functions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/9 下午10:14
+# @Author  : Aries
+# @Site    : 
+# @File    : py025_string_functions.py
+# @Software: PyCharm
+import string
+
+'''
+python string 内建函数
+'''
+
+# 输出0-9
+print string.digits
+print type(string.digits)
+
+# 输出小写的a-z
+print string.lowercase
+print type(string.lowercase)
+
+# 输出空格符
+print string.whitespace
+print len(string.whitespace)  # 长度为6
+print type(string.whitespace)
