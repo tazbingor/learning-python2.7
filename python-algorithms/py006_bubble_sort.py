f910139f4965d831f8e07ac06b7ed23dc57edfc9
jieyan <852353298@qq.com>
2017-11-10 16:42:18 +0800

    python bubble sort logogram

--- python-algorithms/py006_bubble_sort.py
+++ python-algorithms/py006_bubble_sort.py
@@ -13,14 +13,14 @@
 arr = [65, 29, 6, 54, 62, 88, 66, 108]
 count = len(arr)
 i = 0
-
 while i < count - 1:
     j = 0
-
-    while arr[j] > arr[j + 1]:  # 数组中,前一位大于后一位,则进行交换
-        temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
+    while j < count - 1 - i:
+        temp = 0
+        if arr[j] > arr[j + 1]:  # 数组中,前一位大于后一位,则进行交换
+            temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
         j += 1
     i += 1
 
