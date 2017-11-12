57564da062f3612e3292cdb386cfb3f6ad76a536
jieyan <852353298@qq.com>
2017-11-12 19:26:16 +0800

    python bubble sort logogram

--- python-algorithms/py008_Insert_sort.py
+++ python-algorithms/py008_Insert_sort.py
@@ -9,15 +9,15 @@
 插入排序
 '''
 
-array = [3, 49, 100, 66, 24, 99, 76, 0, 88, 56]
+arr = [3, 49, 100, 66, 24, 99, 76, 0, 88, 56]
 
-for i in range(1, len(array)):
-    if array[i - 1] > array[i]:
-        temp = array[i]  # 当前需要排序的元素
+for i in range(1, len(arr)):
+    if arr[i - 1] > arr[i]:
+        temp = arr[i]  # 当前需要排序的元素
         index = i  # 用来记录排序元素需要插入的位置
-        while index > 0 and array[index - 1] > temp:
-            array[index] = array[index - 1]  # 把已经排序好的元素后移一位，留下需要插入的位置
+        while index > 0 and arr[index - 1] > temp:
+            arr[index] = arr[index - 1]  # 把已经排序好的元素后移一位，留下需要插入的位置
             index -= 1
-        array[index] = temp  # 把需要排序的元素，插入到指定位置
+        arr[index] = temp  # 把需要排序的元素，插入到指定位置
 
-print array
+print arr
