4152354e62d822234a7a8919d1248ba61223682f
jieyan <852353298@qq.com>
2017-11-13 16:08:52 +0800

    python string functions unicode

--- python-basics/py26_unicode_uniFile.py
+++ python-basics/py26_unicode_uniFile.py
@@ -24,3 +24,14 @@ bytes_in = f.read()
 f.close()
 hello_in = bytes_in.encode(CODEC)
 print hello_in,
+
+
+'''
+unicode 在实际应用中的原则
+1. 程序中出现字符串时,一定要加前缀u
+2. 不要使用str(), 用unicode代替
+3. 不要使用过时的string模块
+4. 不到必要时不要在程序中编码unicod的,只有在写入文件或者操作数据库时猜调用encode函数
+    相应的,读取数据时才调用decode
+
+'''
