2c946bf7a38a2178172dbe9de5f460e881653f5e
jieyan <852353298@qq.com>
2017-11-14 18:56:45 +0800

    python list stack

--- python-algorithms/py010_list_stack.py
+++ python-algorithms/py010_list_stack.py
@@ -17,6 +17,11 @@
 
 
 other :
+手动实现一下功能
+删除列表末尾元素
+
+
+
 pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
 strip() 方法用于移除字符串头尾指定的字符（默认为空格）。
 '''
@@ -29,12 +34,23 @@ def popit():
     if len(myListStack) == 0:
         print '该列表为空,请添加新的字符串元素!'
     else:
-        print '删除了[', [myListStack.pop()], ']元素'
+        print '删除了[', [myPop(myListStack)], ']元素'
 
 
 # 压栈
 def pushit():
     myListStack.append(raw_input('请输入你要添加的字符串:\n').strip())
+
+
+# 删除末尾元素
+def myPop(str_List):
+    str_List = list(reversed(str_List))
+    result = str_List[0]
+    str_List.remove(str_List[0])
+    # print temp
+    global myListStack
+    myListStack = list(reversed(str_List))
+    return result
 
 
 def showStack():
