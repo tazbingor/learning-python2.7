3daeb0f08622ad3f01f820e698293b53784d0ff6
jieyan <852353298@qq.com>
2017-11-17 20:09:28 +0800

    python

---
+++ python-exercise/py048_multiple_columns_output.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/17 下午8:08
+# @Author  : Aries
+# @Site    : 
+# @File    : py048_multiple_columns_output.py
+# @Software: PyCharm
+'''
+多列输出
+
+有任意项的序列或者其他容器,把它们等距离分列显示.由调用者提供数据和输出格式.
+例如,如果你传入100 个项并定义3 列输出,按照需要的模式显示这些数据.这种情况下,
+应该是两列显示33 个项,最后一列显示34 个.你可以让用户来选择水平排序或者垂直排序.
+'''
+pass
