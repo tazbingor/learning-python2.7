5d856cd9470a8cc4fb83b04d89a0e207c0751587
jieyan <852353298@qq.com>
2017-11-18 17:21:40 +0800

    python aggregate

---
+++ python-basics/unit05-aggregate/py038_aggregate_functions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/18 下午5:20
+# @Author  : Aries
+# @Site    : 
+# @File    : py038_aggregate_functions.py
+# @Software: PyCharm
+'''
+集合类型内建函数
+'''
