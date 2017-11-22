6f54d4963eb34562c060843dfeb1b67e5dcd4e12
jieyan <852353298@qq.com>
2017-11-22 14:07:08 +0800

    python exercises

---
+++ python-exercise/py061_conditional_statement.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/22 下午2:02
+# @Author  : Aries
+# @Site    : 
+# @File    : py061_conditional_statement.py
+# @Software: PyCharm
+'''
+8-1 条件语句,请看下边的代码
+'''
+x = -1
+# statement A
+if x > 0:
+    # statement B
+    pass
+
+elif x < 0:
+    # statement C
+
+    pass
+
+else:
+    # statement D
+    pass
+# statement E
+
+
+'''
+a) 如果x<0,上面哪个语句(A,B,C,D,E)将被执行?
+    C 
+b) 如果x==0,上面哪个语句将被执行?
+    D 
+c) 如果x>0,上面哪个语句将被执行?
+    B
+'''
