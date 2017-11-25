056a4f60b97988b125710d67aaff5948bbd82293
jieyan <852353298@qq.com>
2017-11-25 15:59:10 +0800

    python file

---
+++ python-basics/unit08-file-operations/py051_text_file_read_and_write_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/25 下午3:51
+# @Author  : Aries
+# @Site    : 
+# @File    : py051_text_file_read_and_write_1.py
+# @Software: PyCharm
+'''
+python 文本文件读写方式1
+读一行写一行
+'''
+
+# 打开文件
+file1 = open('poem.txt', 'r')
+file2 = open('new poem.txt', 'w')
+
+while True:
+    line = file1.readline()
+    file2.write(line[:])
+    if not line:
+        break
+
+file1.close()
+file2.close()
