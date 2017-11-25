2fe114c35b82e8444c5a1527e02897553703b468
jieyan <852353298@qq.com>
2017-11-25 16:02:47 +0800

    python file

--- python-basics/unit08-file-operations/py051_text_file_read_and_write_1.py
+++ python-basics/unit08-file-operations/py051_text_file_read_and_write_1.py
@@ -10,15 +10,17 @@ python 文本文件读写方式1
 读一行写一行
 '''
 
-# 打开文件
+# 打开文件流
 file1 = open('poem.txt', 'r')
 file2 = open('new poem.txt', 'w')
 
 while True:
+    # 读写文件
     line = file1.readline()
     file2.write(line[:])
     if not line:
         break
 
+# 关闭文件流
 file1.close()
 file2.close()
