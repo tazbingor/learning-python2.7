7a652f878b3105fe8bf2f59b93f9954ee4c03578
jieyan <852353298@qq.com>
2017-11-28 22:37:21 +0800

    python file screening

--- python-exercise/py074_file_reference.py
+++ python-exercise/py074_file_reference.py
@@ -5,3 +5,15 @@
 # @Site    : 
 # @File    : py074_file_reference.py
 # @Software: PyCharm
+'''
+9-2 文件访问,提示输入数字N和文件F,然后显示文件F的钱N行
+
+'''
+
+
+def fileReference(num, file1):
+    if isinstance(num, 'int') and isinstance(file1, 'file'):
+        return '请输入正确的参数类型'
+
+
+    pass
