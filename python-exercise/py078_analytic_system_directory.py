dbfdf7060290313c7e235059ee192c7fce208b0b
tazdingo <tazdingbean@outlook.com>
2017-11-29 19:54:47 +0800

    commit file

--- python-exercise/py078_analytic_system_directory.py
+++ python-exercise/py078_analytic_system_directory.py
@@ -6,5 +6,21 @@
 # @File    : py078_analytic_system_directory.py
 # @Software: PyCharm
 '''
-9-7 系统解析
+9-7 系统解析系统配置文件
+
 '''
+
+import platform  # 确定当前操作系统
+
+sys = platform.system()
+# print sys
+if sys == 'Windows':
+    pass
+
+elif sys == 'Linux':
+    pass
+
+elif sys == 'MacOS':
+    pass
+
+print dir(sys)
