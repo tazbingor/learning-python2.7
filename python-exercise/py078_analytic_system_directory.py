498d6696fd4ab726d99fa90f9fe6b2e24cdac59c
jieyan <852353298@qq.com>
2017-11-29 19:41:21 +0800

    python file

---
+++ python-exercise/py078_analytic_system_directory.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/29 下午7:40
+# @Author  : Aries
+# @Site    : 
+# @File    : py078_analytic_system_directory.py
+# @Software: PyCharm
+'''
+9-7 系统解析
+'''
