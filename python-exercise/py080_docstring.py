5f531291204f0fb2be57685dcb11b798de162ca8
jieyan <852353298@qq.com>
2017-11-30 19:07:29 +0800

    python file

---
+++ python-exercise/py080_docstring.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/11/30 下午6:52
+# @Author  : Aries
+# @Site    : 
+# @File    : py080_docstring.py
+# @Software: PyCharm
+'''
+9-9 文档字符串
+进入python标准库所在的目录.检查每一个.py文件是否有_doc_字符串,如果有,对其格式进行适当的整理归类.
+你的程序执行完毕后,应该会生成一个漂亮的清单.里面列出哪些模块有文档字符串,以及文档字符串的内容,清单最后附上哪些没有文档字符串模块的名字
+附加题:提取标准库中各个模块内的全部class和函数的文档
+'''
+
+
+def get_doc_library(pyPath):
+    pass
+
+
+def get_mod_class(pyPath):
+    pass
+
+
+def get_mod_functions(pyPath):
+    pass
+
+
+import sys
+
+for i in sys.path:
+    print  i
