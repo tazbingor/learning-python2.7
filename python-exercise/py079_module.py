397eb6dbaa7b1ccaee043e3dedb7b874db9a84a0
jieyan <852353298@qq.com>
2017-11-30 18:52:16 +0800

    python file

--- python-exercise/py079_module.py
+++ python-exercise/py079_module.py
@@ -7,8 +7,26 @@
 # @Software: PyCharm
 '''
 9-8模块研究
-提取模块的属性资料.提示用户输入一个模块名.然后使用dir()和其他函数提取模块属性,显示它们的名字,类型 值
+提取模块的属性资料.提示用户输入一个模块名.然后使用dir()和其他函数提取模块属性,显示它们的名字,类型,值
 '''
 
-for i in dir(str()):
-    print i
+
+def module_details(strModule):
+    modules = __import__(strModule)
+    mod = dir(modules)
+    # print mod
+    names = []
+    mtypes = []
+    values = []
+    for i in mod:
+        names.append(i)
+        mtypes.append(type(getattr(mod, i)))
+        values.append(getattr(mod, i))
+    return [names, mtypes, values]
+
+
+user_input = raw_input('请输入模块名:\n')
+mod_results = module_details(user_input)
+print '模块名: ', mod_results[0]
+print '类型为: ', mod_results[1]
+print '值为: ', mod_results[2]
