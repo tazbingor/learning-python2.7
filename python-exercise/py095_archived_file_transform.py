d64aa32d88bb75045022321edeb5297370584375
jieyan <852353298@qq.com>
2017-12-06 18:46:44 +0800

    python file

---
+++ python-exercise/py095_archived_file_transform.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/6 下午6:44
+# @Author  : Aries
+# @Site    : 
+# @File    : py095_archived_file_transform.py
+# @Software: PyCharm
+'''
+9–24. 归档文件转换.
+参考前两个问题的解决方案, 写一个程序, 在 ZIP (.zip) 和TAR/gzip (.tgz/.tar.gz) 或 TAR/bzip2 (.tbz/.tar.bz2) 归档文件间移动文件.
+ 文件可能是已经存在的, 必要时请创建文件.
+'''
+pass
---
+++ python-exercise/py096_ dekompresor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/6 下午6:45
+# @Author  : Aries
+# @Site    : 
+# @File    : py096_ dekompresor.py
+# @Software: PyCharm
+'''
+9–25. 通用解压程序.
+创建一个程序, 接受任意数目的归档文件以及一个目标目录做为参数.
+归档文件格式可以是 .zip, .tgz, .tar.gz, .gz, .bz2, .tar.bz2, .tbz 中的一种或几种.
+程序会把第一个归档文件解压后放入目标目录, 把其它归档文件解压后放入以对应文件名命名的目录下(不包括扩展名).
+例如输入的文件名为 header.txt.gz 和 data.tgz ， 目录为 incoming ,header.txt 会被解压到 incoming
+而 data.tgz 中的文件会被放入 incoming/data .
+'''
+pass
