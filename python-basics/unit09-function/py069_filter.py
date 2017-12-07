66e3f466b4130f1870feff8e41b21735708717f1
jieyan <852353298@qq.com>
2017-12-07 17:22:22 +0800

    python function lambda

--- python-basics/unit09-function/py069_filter.py
+++ python-basics/unit09-function/py069_filter.py
@@ -5,3 +5,17 @@
 # @Site    : 
 # @File    : py069_filter.py
 # @Software: PyCharm
+'''
+filter()
+调用一个布尔函数func来迭代遍历每个seq中的元素;返回一个使func返回值为true的元素序列
+相当于一个筛子用来过滤一个函数
+'''
+
+
+# 实现filter
+def myFilter(bool_func, seq):
+    result_seq = []
+    for eachItem in seq:
+        if bool_func(eachItem):
+            result_seq.append(eachItem)
+    return result_seq
