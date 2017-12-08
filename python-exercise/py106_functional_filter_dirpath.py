decabdfb434cbd389e05bcaf21b22e99afbb9cc7
jieyan <852353298@qq.com>
2017-12-08 18:42:05 +0800

    python exercise

---
+++ python-exercise/py106_functional_filter_dirpath.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/8 下午6:29
+# @Author  : Aries
+# @Site    : 
+# @File    : py106_functional_filter_dirpath.py
+# @Software: PyCharm
+'''
+11-10.
+用filter()进行函数式编程。在unix文件系统中，在每个文件夹或者目录中都有两个特别的文件："."表示现在的目录，".."表示父目录。
+给出上面的知识，看一下os.listdir()函数的文档并描述这段代码做了什么：
+files = filter(lambda x: x and x[0] != '.', os.listdir(folder))
+'''
+import os
+
+# 假设folder是path
+folder = 'rising-python-classics/python-algorithms'
+
+if __name__ == '__main__':
+    files = filter(lambda x: x and x[0] != '.', os.listdir(folder))
+    print files  # 返回的是当前文件夹(路径)下的所有文件的列表
+
+
---
+++ python-exercise/py107_functional_map_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/8 下午6:27
+# @Author  : Aries
+# @Site    : 
+# @File    : py107_functional_map_file.py
+# @Software: PyCharm
+'''
+
+'''
