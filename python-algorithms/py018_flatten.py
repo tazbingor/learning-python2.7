d5b1e5454f67dc52baf166d09ab9f7f028454de7
jieyan <852353298@qq.com>
2017-12-08 15:24:35 +0800

    python function

---
+++ python-algorithms/py018_flatten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/8 下午3:17
+# @Author  : Aries
+# @Site    : 
+# @File    : py088_flatten.py
+# @Software: PyCharm
+'''
+生成器实现列表降维
+'''
+
+numList = [1, 2, [3, 4, [5, 6], [7, 8, [9, 10]], 11], 12]
+
+
+def flatten(alist):
+    for eachItem in alist:
+        if isinstance(eachItem, list):
+            for i in flatten(eachItem):
+                yield i
+        else:
+            yield eachItem
+
+
+if __name__ == '__main__':
+    print numList
