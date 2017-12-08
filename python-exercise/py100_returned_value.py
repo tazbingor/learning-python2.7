7ed54b7ff7a94e5696f8b0947a2961466d4d8545
jieyan <852353298@qq.com>
2017-12-08 17:16:18 +0800

    python exercise

---
+++ python-exercise/py100_returned_value.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/8 下午4:48
+# @Author  : Aries
+# @Site    : 
+# @File    : py100_returned_value.py
+# @Software: PyCharm
+'''
+11–4. 返回值。
+给你在5-13 的解创建一个补充函数。创建一个带以分为单位的总时间以及返回一个以小时和分为单位的等价的总时间。
+
+'''
+
+
+def getTime(min):
+    return '%sh:%sm' % (min / 60, min % 60)
+
+
+if __name__ == '__main__':
+    print getTime(280)
+    print getTime(180)
---
+++ python-exercise/py101_default_parameters.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/8 下午5:08
+# @Author  : Aries
+# @Site    : 
+# @File    : py101_default_parameters.py
+# @Software: PyCharm
+'''
+11-5. 默认参数。
+更新你在练习5-7中创建的销售税脚本以便让销售税率不再是函数的必要参数。使用你的地方税率作为默认参数。
+
+'''
+
+
+def business_tax(pureprice, rate=0.10):
+    return pureprice * rate
+
+
+if __name__ == '__main__':
+    print '计算明朝商业税'
+    print business_tax(300)
