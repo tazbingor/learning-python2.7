e9c3d4200dc8c90bb1e785c3b087999bc1feef2e
tazdingo <tazdingbean@outlook.com>
2017-12-13 22:30:10 +0800

    commit file

---
+++ python-basics/unit10-class/py092_instance.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/13 下午 10:29
+# @Author  : Aries
+# @Site    : 
+# @File    : py092_instance.py
+# @Software: PyCharm
+'''
+实例
+'''
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
