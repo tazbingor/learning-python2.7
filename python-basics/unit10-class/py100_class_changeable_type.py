2e06d5185d121ce23b5215eaf214b3105dc6beec
jieyan <852353298@qq.com>
2017-12-19 17:10:02 +0800

    python class

--- python-basics/unit10-class/py100_class_changeable_type.py
+++ python-basics/unit10-class/py100_class_changeable_type.py
@@ -3,7 +3,7 @@
 # @Time    : 17/12/19 下午4:15
 # @Author  : Aries
 # @Site    : 
-# @File    : py100_class_Immutable_type.py
+# @File    : py100_class_changeable_type.py
 # @Software: PyCharm
 '''
 可变类型
@@ -20,7 +20,7 @@ class SortedKeyDict(dict):
 # 映射类型子类化
 class SortedNewKeyDict(dict):
     def keys(self):
-        return sorted(self.keys())
+        return sorted(self.keys())  # 这种方式属于递归
 
 
 if __name__ == '__main__':
@@ -29,6 +29,5 @@ if __name__ == '__main__':
     print temp_dict.keys()  # ['A', 'B', 'C']
 
     print '-' * 50
-    temp_dict1 = SortedNewKeyDict((('C', 3), ('B', 2), ('A', 1)))
-    print temp_dict1.keys() # RuntimeError: maximum recursion depth exceeded
-
+    # temp_dict1 = SortedNewKeyDict((('C', 3), ('B', 2), ('A', 1)))
+    # print temp_dict1.keys()  # RuntimeError: maximum recursion depth exceeded
