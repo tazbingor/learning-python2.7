d1f85c9bfa48eb0df1a46b1143d2c816ae23121a
jieyan <852353298@qq.com>
2017-12-21 16:06:37 +0800

    python class

--- python-basics/unit10-class/py109_anyiter.py
+++ python-basics/unit10-class/py109_anyiter.py
@@ -12,4 +12,35 @@
 
 
 class AnyIter(object):
-    pass
+    def __init__(self, data, safe=False):
+        self.safe = safe
+        self.iter = iter(data)
+
+    def __iter__(self):
+        return self
+
+    def next(self, howmany=1):
+        retval = []
+        for eachItem in range(howmany):
+            try:
+                retval.append(self.iter.next())
+            except StopIteration:
+                if self.safe:
+                    break
+                else:
+                    raise
+        return retval
+
+
+if __name__ == '__main__':
+    a = AnyIter(range(10))
+    i = iter(a)
+    for j in range(1, 5):
+        print j, ':', i.next(j)
+
+    '''
+    1 : [0]
+    2 : [1, 2]
+    3 : [3, 4, 5]
+    4 : [6, 7, 8, 9]   
+    '''
