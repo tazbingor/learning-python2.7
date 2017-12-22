0587ff35ff4886fb7a8a45fa3a6f14c64466b052
jieyan <852353298@qq.com>
2017-12-22 19:32:02 +0800

    python class

---
+++ python-basics/unit10-class/py110_multitype_customization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/22 下午7:31
+# @Author  : Aries
+# @Site    : 
+# @File    : py110_multitype_customization.py
+# @Software: PyCharm
+'''
+多类型定制
+'''
+
