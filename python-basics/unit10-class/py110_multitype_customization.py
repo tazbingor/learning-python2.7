925b9212fc1546e4e0145f76ecf22d6b07e22f6d
jieyan <852353298@qq.com>
2017-12-22 19:52:46 +0800

    python class

--- python-basics/unit10-class/py110_multitype_customization.py
+++ python-basics/unit10-class/py110_multitype_customization.py
@@ -29,3 +29,59 @@ class NumberStr(object):
         else:
             raise TypeError, 'TYPE ERROR'
 
+    def __mul__(self, num):
+        if isinstance(num, int):
+            return self.__class__(
+                self.__num * num,
+                self.__string * num)
+        else:
+            raise TypeError, 'TYPE ERROR'
+
+    def __nonzero__(self):
+        return self.__num or len(self.__string)
+
+    def __norm_cval(self, cmpres):
+        return cmp(cmpres, 0)
+
+    def __cmp__(self, other):
+        return self.__norm_cval(cmp(self.__num, other.__num)) + \
+               self.__norm_cval(cmp(self.__string, other.__string))
+
+
+def main():
+    a = NumberStr(3, 'foo')
+    b = NumberStr(3, 'goo')
+    c = NumberStr(2, 'foo')
+    d = NumberStr()
+    e = NumberStr(string='boo')
+    f = NumberStr(1)
+
+    print a  # [3 :: 'foo']
+
+    print b  # [3 :: 'goo']
+
+    print c  # [2 :: 'foo']
+
+    print d  # [0 :: '']
+
+    print e  # [0 :: 'boo']
+
+    print f  # [1 :: '']
+
+    print a < b
+
+    print b < c  # False
+
+    print a == a
+
+    print b * 2  # [6 :: 'googoo']
+
+    print a * 3  # [9 :: 'foofoofoo']
+
+    print b + e  # [3 :: 'gooboo']
+
+    print e + b  # [3 :: 'boogoo']
+
+
+if __name__ == '__main__':
+    main()
