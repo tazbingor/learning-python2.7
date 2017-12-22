d7128afcb4de462c84106d96c87a7f8b9f2d8cea
jieyan <852353298@qq.com>
2017-12-22 19:37:32 +0800

    python class

--- python-basics/unit10-class/py110_multitype_customization.py
+++ python-basics/unit10-class/py110_multitype_customization.py
@@ -9,3 +9,23 @@
 多类型定制
 '''
 
+
+class NumberStr(object):
+    def __init__(self, num=0, string=''):
+        self.__num = num
+        self.__string = string
+
+    def __str__(self):
+        return '[%d :: %r]' % \
+               (self.__num, self.__string)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        if isinstance(other, NumberStr):
+            return self.__class__(
+                self.__num + other.__num,
+                self.__string + other.__string)
+        else:
+            raise TypeError, 'TYPE ERROR'
+
