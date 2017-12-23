836bc77f19d12a2b4b36bcf29e9688eaf0a506df
jieyan <852353298@qq.com>
2017-12-23 18:53:42 +0800

    python class

---
+++ python-exercise/3-class/py113_program_design.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/23 下午6:47
+# @Author  : Aries
+# @Site    : 
+# @File    : py113_program_design.py
+# @Software: PyCharm
+'''
+13-1 程序设计.请列举一些面向对象编程与传统的程序设计形式相比的先进之处
+
+面向对象的特点: 封装,继承,多态
+总而言之,就是将传统面向过程的编程思路进一步抽象,使得代码更加简洁
+'''
+
