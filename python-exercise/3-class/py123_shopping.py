43e6676583afc459b461f5f1f040d6362d47e9eb
tazdingobean <jieyan@jiedeMacBook-Pro.local>
2017-12-26 15:47:38 +0800

    python class

--- python-exercise/3-class/py123_shopping.py
+++ python-exercise/3-class/py123_shopping.py
@@ -39,5 +39,12 @@ class Cart(object):
 
 
 if __name__ == '__main__':
-    user_01 = User('张三', 1)
-    user_02 = User('李四', 2)
+    # 购物清单
+    item1 = Item(arg1='苹果', arg2='香蕉', arg3='山药')
+    item2 = Item(arg1='牛蹄筋', arg2='牛排', arg3='八角', arg4='小茴香', arg5='瓦罐')
+    # 购物车
+    cart1 = Cart(item1, item2)
+    cart2 = Cart(item1)
+    # 顾客
+    user_01 = User('张三', 1, cart1, cart2)
+    user_02 = User('李四', 2, cart2)
