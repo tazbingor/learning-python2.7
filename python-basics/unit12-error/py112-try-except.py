bb1f6ee47a2e271bce691cbe51e8ef7f6a50c17a
tazdingobean <jieyan@jiedeMacBook-Pro.local>
2017-12-28 22:43:19 +0800

    python error

--- python-basics/unit12-error/py112-try-except.py
+++ python-basics/unit12-error/py112-try-except.py
@@ -19,7 +19,7 @@ def open_file(path, opt='rb'):
 
 if __name__ == '__main__':
     f = open_file('something')  # 文件输入输出操作异常: [Errno 2] No such file or directory: 'something'
-
+    pass
 '''
 注意: try语句中的某一句一旦出现异常,那么try中的剩余代码将被忽略,
 转而执行 except 的处理异常操作
