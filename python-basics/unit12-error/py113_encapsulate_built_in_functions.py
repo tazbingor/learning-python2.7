a6b7c991d494a5f26188162cbd2f109d16f59be7
tazdingobean <jieyan@jiedeMacBook-Pro.local>
2017-12-28 23:28:44 +0800

    python error

---
+++ python-basics/unit12-error/py113_encapsulate_built_in_functions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/28 下午11:26
+# @Author  : Aries
+# @Site    : 
+# @File    : py113_encapsulate_built_in_functions.py
+# @Software: PyCharm
+'''
+封装內建函数
+
+'''
+
+
+def safe_float(obj):
+    try:
+        retval = float(obj)
+    except ValueError:
+        retval = 'could not convert non-number to float'
+    return retval
+
+
+if __name__ == '__main__':
+    print safe_float('12.34')  # 12.34
+    print safe_float('bad input') # could not convert non-number to float
+
