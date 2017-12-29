fb416878dcad3d0f6712901e051ae13e61c66591
tazbingor <852353298@qq.com>
2017-12-29 17:20:04 +0800

    python test

--- python-basics/unit12-error/py113_encapsulate_built_in_functions.py
+++ python-basics/unit12-error/py113_encapsulate_built_in_functions.py
@@ -18,7 +18,7 @@ def safe_float(obj):
         retval = 'could not convert non-number to float'
     return retval
 
+
 if __name__ == '__main__':
     print safe_float('12.34')  # 12.34
-    print safe_float('bad input') # could not convert non-number to float
-
+    print safe_float('bad input')  # could not convert non-number to float
