5aa42a6883f8e8c039870b2401cf6861e793788f
tazbingor <852353298@qq.com>
2017-12-30 22:58:31 +0800

    python except

---
+++ python-basics/unit12-error/py114_multiple_except.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 17/12/30 下午10:53
+# @Author  : Aries
+# @Site    : 
+# @File    : py114_multiple_except.py
+# @Software: PyCharm
+'''
+带有许多except 的try语句
+'''
+
+
+def safe_float(obj):
+    try:
+        retval = float(obj)
+    except ValueError:
+        retval = 'could not convert non-number to float'
+    except TypeError:
+        retval = 'object type connot be converted to float'
+    return retval
+
+
+if __name__ == '__main__':
+    print safe_float('xyz')  # could not convert non-number to float
+
+    print safe_float([])  # object type connot be converted to float
+
+    print safe_float(())  # object type connot be converted to float
+
+    print safe_float(200)  # 200.0
+
+    print  safe_float(89.69)  # 89.69
