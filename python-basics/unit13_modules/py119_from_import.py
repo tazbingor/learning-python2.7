6cbe9fed4e5860d6a1b036d562ca71085695a158
tazbingor <852353298@qq.com>
2018-01-04 22:09:46 +0800

    python except

---
+++ python-basics/unit13_modules/py119_from_import.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/4 下午10:06
+# @Author  : Aries
+# @Site    : 
+# @File    : py119_from_import.py
+# @Software: PyCharm
+'''
+from .. import 语句
+多行导入
+'''
+from Tkinter import Tk, Frame, Button, Entry, \
+    Canvas, Text, LEFT
+
+'''
+PEP 8风格
+加入括号更易读
+'''
+from Tkinter import (Tk, Frame, Button, Entry, Canvas, Text, LEFT)
