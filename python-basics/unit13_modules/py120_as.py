7e97ab6dfea013d9fefc5d06e365e10be195e8ac
tazbingor <852353298@qq.com>
2018-01-04 22:12:53 +0800

    python import

---
+++ python-basics/unit13_modules/py120_as.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/4 下午10:10
+# @Author  : Aries
+# @Site    : 
+# @File    : py120_as.py
+# @Software: PyCharm
+'''
+as: 拓展import语句
+'''
+import Tkinter
+from cgi import FieldStorage
+# 替换为..
+import Tkinter as tk
+from cgi import FieldStorage as form
+
