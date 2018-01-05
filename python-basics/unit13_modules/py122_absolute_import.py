86cb030d53a8dcf97d6a4adff6ab6aabad778f82
tazbingor <852353298@qq.com>
2018-01-05 23:28:15 +0800

    python import

---
+++ python-basics/unit13_modules/py122_absolute_import.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/5 下午11:26
+# @Author  : Aries
+# @Site    : 
+# @File    : py122_absolute_import.py
+# @Software: PyCharm
+'''
+绝对导入
+'''
+import sys
+
+if __name__ == '__main__':
+    print sys.path  # 系统路径列表
