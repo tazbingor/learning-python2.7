ff4f421f9f2ccd2e2c45af977c0d74899feb00c9
tazbingor <852353298@qq.com>
2018-01-06 20:36:38 +0800

    python import

---
+++ python-exercise/5-modules/mymodule.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/6 下午8:24
+# @Author  : Aries
+# @Site    : 
+# @File    : mymodule.py
+# @Software: PyCharm
+def foo():
+    print 'mymodule下的foo函数'
--- python-exercise/5-modules/py142_Import_properties.py
+++ python-exercise/5-modules/py142_Import_properties.py
@@ -6,5 +6,20 @@
 # @File    : py142_Import_properties.py
 # @Software: PyCharm
 '''
+12-2. 导入属性.假设你的模块mymodule中有一个foo()函数:
+    1.把这个函数导入到你的命名空间有哪两种方法?
+    2.这两种方法有何异同?
 
+'''
+import mymodule  # 绝对导入
+from mymodule import foo  # 相对导入
+
+if __name__ == '__main__':
+    mymodule.foo()  # mymodule下的foo函数
+    foo()  # mymodule下的foo函数
+
+'''
+1.导入命名空间的方法有绝对导入和相对导入
+2.绝对导入,使用'import 空间名称' 的方式,将mymodule内的属性全部导入.
+  相对导入,使用'from 属性/函数名 import 空间名'的方式,只是导入mymodule中的foo函数
 '''
