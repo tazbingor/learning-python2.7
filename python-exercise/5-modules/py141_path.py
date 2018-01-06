47c3e05655cfd4ca1e7a846a802c3dafe33fffef
tazbingor <852353298@qq.com>
2018-01-06 20:22:27 +0800

    python import

---
+++ python-exercise/5-modules/py141_path.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/6 下午8:09
+# @Author  : Aries
+# @Site    : 
+# @File    : py141_path.py
+# @Software: PyCharm
+'''
+12-1 路径搜索和搜索路径有什么不同?
+
+'''
+'''
+路径搜索:指查找某一个文件的操作
+搜索路径:指查找目录(path)
+'''
---
+++ python-exercise/5-modules/py142_Import_properties.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/6 下午8:22
+# @Author  : Aries
+# @Site    : 
+# @File    : py142_Import_properties.py
+# @Software: PyCharm
+'''
+
+'''
