479c7105fd9c02df9f855accb92c1b5203d427a2
tazbingor <852353298@qq.com>
2018-01-07 23:33:57 +0800

    python modules

---
+++ python-advanced/unit01-regular-expression/py01_first_regex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/6 下午9:03
+# @Author  : Aries
+# @Site    : 
+# @File    : py01_first_regex.py
+# @Software: PyCharm
+'''
+第一个正则表达式
+'''
+import re
+
+str = 'an example word:cat!'
+match = re.search(r'world:\w\w\w\w', str)
+if match:
+    print 'found', match.group()
+else:
+    print 'did not find'
