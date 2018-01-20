d68e454551acce745fa9e6cd5c82ca2369ff90d1
tazbingor <852353298@qq.com>
2018-01-20 23:58:20 +0800

    python  alg

---
+++ python-algorithms/1-algorithm-concept/alg01_concept.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/20 下午11:49
+# @Author  : Aries
+# @Site    : 
+# @File    : alg01_concept.py
+# @Software: PyCharm
+'''
+倘若a+b+c = 1000,且a^2+b^2=c^2 (a,b,c为自然数)
+    求出a,b,c可能的组合
+
+最简单的解法,巨慢无比!
+'''
+import time
+
+
+def get_combination():
+    start_time = time.time()
+    for a in range(0, 1001):
+        for b in range(0, 1001):
+            for c in range(0, 1001):
+                if a + b + c == 1000 and a ** 2 + b ** 2 == c ** 2:
+                    print 'a,b,c:%d,%d,%d' % (a, b, c)
+    end_time = time.time()
+    print 'times:%d' % (end_time - start_time)
+    print 'finished'
+
+
+if __name__ == '__main__':
+    get_combination()
+    '''
+    a,b,c:0,500,500
+    a,b,c:200,375,425
+    a,b,c:375,200,425
+    a,b,c:500,0,500
+    times:67
+    finished
+    '''
