f6b1bf3810c8ac6516eadbfce644f08b0bcf6bb4
tazbingor <852353298@qq.com>
2018-01-25 20:11:04 +0800

    python  alg

---
+++ python-algorithms/2-sequence-list/alg003_sequence_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18/1/25 下午6:42
+# @Author  : Aries
+# @Site    : 
+# @File    : alg003_sequence_list.py
+# @Software: PyCharm
+'''
+顺序表:
+    将元素有顺序的放在一块连续的存储区里,元素间的顺序关系由他们的存储顺序自然表示
+
+一般有两种实现
+    1.一体式
+        表信息和表元素线性共存于一个内存中
+
+    2.分离式
+        表信息和表元素线分别存于两个独立内存空间,通过链接相关联
+
+list基本实现
+    1.基于下标查找元素
+    2.允许任意加入元素
+    3.本质为分离式的动态顺序表
+'''
+
+
+class SequenceList(object):
+    pass
